def4cfe92ff648d51569bfd23f1d54da18fc60e8
Liebniz <anders.leif.loren@liebniz.com>
2024-06-12 14:13:18 +0200

    tests: testBookMetamodel
    All metamodels have been tested for Author and Book.

--- src/test/java/com/liebniz/book/TestBookMetamodel.java
+++ src/test/java/com/liebniz/book/TestBookMetamodel.java
@@ -24,12 +24,16 @@ public class TestBookMetamodel {
         entities.add("Author");
         entities.add("Book");
 
+        System.out.println("Entities in List: ");
+        entities.forEach(System.out::println);
+
         EntityManagerFactory emf = new HibernatePersistenceProvider()
                 .createContainerEntityManagerFactory(customPersistenceUnitInfo, Map.of());
 
         Metamodel mm = emf.getMetamodel();
 
         Set<ManagedType<?>> managedTypes = mm.getManagedTypes();
+        System.out.println("ManagedTypes size: " + managedTypes.size());
         assertEquals(managedTypes.size(), 2);
 
         int counter = 0;
@@ -40,6 +44,7 @@ public class TestBookMetamodel {
             if (managedType.getJavaType().getTypeName().substring(periodPlace, stringLength).equals("Author")) {
 
                 SingularAttribute<?, ?> firstnameAttribute = managedType.getSingularAttribute("firstname");
+                System.out.println(entities.get(counter) + " firstnameAttribute type: " + firstnameAttribute.getJavaType());
                 assertEquals(
                         firstnameAttribute.getJavaType(),
                         String.class
@@ -49,6 +54,7 @@ public class TestBookMetamodel {
                         Attribute.PersistentAttributeType.BASIC
                 );
                 SingularAttribute<?, ?> lastnameAttribute = managedType.getSingularAttribute("lastname");
+                System.out.println(entities.get(counter) + " lastnameAttribute type: " + lastnameAttribute.getJavaType());
                 assertEquals(
                         firstnameAttribute.getJavaType(),
                         String.class
@@ -58,6 +64,40 @@ public class TestBookMetamodel {
                         Attribute.PersistentAttributeType.BASIC
                 );
             }
+            if (managedType.getJavaType().getTypeName().substring(periodPlace, stringLength).equals("Book")) {
+
+                SingularAttribute<?, ?> titleAttribute = managedType.getSingularAttribute("title");
+                System.out.println(entities.get(counter) + " titleAttribute type: " + titleAttribute.getJavaType());
+                assertEquals(
+                        titleAttribute.getJavaType(),
+                        String.class
+                );
+                assertEquals(
+                        titleAttribute.getPersistentAttributeType(),
+                        Attribute.PersistentAttributeType.BASIC
+                );
+                SingularAttribute<?, ?> isbnAttribute = managedType.getSingularAttribute("isbn");
+                System.out.println(entities.get(counter) + " isbnAttribute type: " + isbnAttribute.getJavaType());
+                assertEquals(
+                        isbnAttribute.getJavaType(),
+                        String.class
+                );
+                assertEquals(
+                        isbnAttribute.getPersistentAttributeType(),
+                        Attribute.PersistentAttributeType.BASIC
+                );
+                SingularAttribute<?, ?> editionAttribute = managedType.getSingularAttribute("edition");
+                System.out.println(entities.get(counter) + " editionAttribute type: " + editionAttribute.getJavaType());
+                assertEquals(
+                        editionAttribute.getJavaType(),
+                        String.class
+                );
+                assertEquals(
+                        editionAttribute.getPersistentAttributeType(),
+                        Attribute.PersistentAttributeType.BASIC
+                );
+            }
+            counter++;
         }
     }
 }
